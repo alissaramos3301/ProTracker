fa26f2996b27f956014bf1b7ef342e57fb01182c
Alissa Ramos <alipdx25@gmail.com>
2022-09-15 16:53:58 -0700

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,8 @@ from django.views.generic.list import ListView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
@@ -15,3 +17,12 @@ class ProjectListView(LoginRequiredMixin, ListView):
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "detail.html"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "create.html"
+    fields = ["name", "description", "members"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=[self.object.id])
